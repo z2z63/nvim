454be1abff4b9e156fd3cb00b7917dd584c29bc4
z2z63 <vituralfuture@gmail.com>
2025-07-02 03:00:08 +0800

    补全引擎切换到blink

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -21,73 +21,73 @@ return {
 			},
 		},
 	},
-	{
-		"hrsh7th/nvim-cmp",
-		dependencies = {
-			"hrsh7th/cmp-nvim-lsp",
-			"windwp/nvim-autopairs",
-		},
-		opts = function(_, opts)
-			opts.sources = opts.sources or {}
-			table.insert(opts.sources, {
-				name = "lazydev",
-				group_index = 0, -- set group index to 0 to skip loading LuaLS completions
-			})
-		end,
-		config = function()
-			local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-			local cmp = require("cmp")
-			cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
-			cmp.setup({
-				view = {
-					entries = "custom", -- can be "custom", "wildmenu" or "native"
-				},
-				snippet = {
-					-- REQUIRED - you must specify a snippet engine
-					expand = function(args)
-						-- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-						-- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-						-- require('snippy').expand_snippet(args.body) -- For `snippy` users.
-						-- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
-						-- vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
-
-						-- For `mini.snippets` users:
-						-- local insert = MiniSnippets.config.expand.insert or MiniSnippets.default_insert
-						-- insert({ body = args.body }) -- Insert at cursor
-						-- cmp.resubscribe({ "TextChangedI", "TextChangedP" })
-						-- require("cmp.config").set_onetime({ sources = {} })
-					end,
-				},
-				window = {
-					completion = cmp.config.window.bordered(),
-					documentation = cmp.config.window.bordered(),
-				},
-				mapping = cmp.mapping.preset.insert({
-					["<C-b>"] = cmp.mapping.scroll_docs(-4),
-					["<C-f>"] = cmp.mapping.scroll_docs(4),
-					["<C-Space>"] = cmp.mapping.complete(),
-					["<C-e>"] = cmp.mapping.abort(),
-					["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-				}),
-				sources = cmp.config.sources({
-					{ name = "nvim_lsp" },
-					{ name = "vsnip" }, -- For vsnip users.
-					-- { name = 'luasnip' }, -- For luasnip users.
-					-- { name = 'ultisnips' }, -- For ultisnips users.
-					-- { name = 'snippy' }, -- For snippy users.
-				}, {
-					{ name = "buffer" },
-				}),
-			})
-			-- The nvim-cmp almost supports LSP's capabilities so You should advertise it to LSP servers..
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-
-			-- An example for configuring `clangd` LSP to use nvim-cmp as a completion engine
-			-- require("lspconfig").jdtls.setup({
-			-- 	capabilities = capabilities,
-			-- })
-		end,
-	},
+	-- {
+	-- 	"hrsh7th/nvim-cmp",
+	-- 	dependencies = {
+	-- 		"hrsh7th/cmp-nvim-lsp",
+	-- 		"windwp/nvim-autopairs",
+	-- 	},
+	-- 	opts = function(_, opts)
+	-- 		opts.sources = opts.sources or {}
+	-- 		table.insert(opts.sources, {
+	-- 			name = "lazydev",
+	-- 			group_index = 0, -- set group index to 0 to skip loading LuaLS completions
+	-- 		})
+	-- 	end,
+	-- 	config = function()
+	-- 		local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+	-- 		local cmp = require("cmp")
+	-- 		cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+	-- 		cmp.setup({
+	-- 			view = {
+	-- 				entries = "custom", -- can be "custom", "wildmenu" or "native"
+	-- 			},
+	-- 			snippet = {
+	-- 				-- REQUIRED - you must specify a snippet engine
+	-- 				expand = function(args)
+	-- 					-- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+	-- 					-- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+	-- 					-- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+	-- 					-- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+	-- 					-- vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
+	--
+	-- 					-- For `mini.snippets` users:
+	-- 					-- local insert = MiniSnippets.config.expand.insert or MiniSnippets.default_insert
+	-- 					-- insert({ body = args.body }) -- Insert at cursor
+	-- 					-- cmp.resubscribe({ "TextChangedI", "TextChangedP" })
+	-- 					-- require("cmp.config").set_onetime({ sources = {} })
+	-- 				end,
+	-- 			},
+	-- 			window = {
+	-- 				completion = cmp.config.window.bordered(),
+	-- 				documentation = cmp.config.window.bordered(),
+	-- 			},
+	-- 			mapping = cmp.mapping.preset.insert({
+	-- 				["<C-b>"] = cmp.mapping.scroll_docs(-4),
+	-- 				["<C-f>"] = cmp.mapping.scroll_docs(4),
+	-- 				["<C-Space>"] = cmp.mapping.complete(),
+	-- 				["<C-e>"] = cmp.mapping.abort(),
+	-- 				["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+	-- 			}),
+	-- 			sources = cmp.config.sources({
+	-- 				{ name = "nvim_lsp" },
+	-- 				{ name = "vsnip" }, -- For vsnip users.
+	-- 				-- { name = 'luasnip' }, -- For luasnip users.
+	-- 				-- { name = 'ultisnips' }, -- For ultisnips users.
+	-- 				-- { name = 'snippy' }, -- For snippy users.
+	-- 			}, {
+	-- 				{ name = "buffer" },
+	-- 			}),
+	-- 		})
+	-- 		-- The nvim-cmp almost supports LSP's capabilities so You should advertise it to LSP servers..
+	-- 		local capabilities = require("cmp_nvim_lsp").default_capabilities()
+	--
+	-- 		-- An example for configuring `clangd` LSP to use nvim-cmp as a completion engine
+	-- 		-- require("lspconfig").jdtls.setup({
+	-- 		-- 	capabilities = capabilities,
+	-- 		-- })
+	-- 	end,
+	-- },
 	{
 		"nvim-tree/nvim-tree.lua",
 		version = "*",
@@ -235,5 +235,58 @@ return {
 		},
 		---@type bootls.Config
 		opts = {},
+	},
+	{
+		"saghen/blink.cmp",
+		-- optional: provides snippets for the snippet source
+		dependencies = { "rafamadriz/friendly-snippets" },
+
+		-- use a release tag to download pre-built binaries
+		version = "1.*",
+		-- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
+		-- build = 'cargo build --release',
+		-- If you use nix, you can build from source using latest nightly rust with:
+		-- build = 'nix run .#build-plugin',
+
+		---@module 'blink.cmp'
+		---@type blink.cmp.Config
+		opts = {
+			-- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
+			-- 'super-tab' for mappings similar to vscode (tab to accept)
+			-- 'enter' for enter to accept
+			-- 'none' for no mappings
+			--
+			-- All presets have the following mappings:
+			-- C-space: Open menu or open docs if already open
+			-- C-n/C-p or Up/Down: Select next/previous item
+			-- C-e: Hide menu
+			-- C-k: Toggle signature help (if signature.enabled = true)
+			--
+			-- See :h blink-cmp-config-keymap for defining your own keymap
+			keymap = { preset = "super-tab" },
+
+			appearance = {
+				-- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+				-- Adjusts spacing to ensure icons are aligned
+				nerd_font_variant = "mono",
+			},
+
+			-- (Default) Only show the documentation popup when manually triggered
+			completion = { documentation = { auto_show = false } },
+
+			-- Default list of enabled providers defined so that you can extend it
+			-- elsewhere in your config, without redefining it, due to `opts_extend`
+			sources = {
+				default = { "lsp", "path", "snippets", "buffer" },
+			},
+
+			-- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
+			-- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
+			-- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
+			--
+			-- See the fuzzy documentation for more information
+			fuzzy = { implementation = "prefer_rust_with_warning" },
+		},
+		opts_extend = { "sources.default" },
 	},
 }
